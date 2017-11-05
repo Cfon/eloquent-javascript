97f313a5d64a81d94e265128864d870be3089ee3
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-05 18:02:23 +0800

    feat: add export and sync for chapters

--- src/models/chapter.ts
+++ src/models/chapter.ts
@@ -1,8 +1,10 @@
 'use strict'
 
 import db from '../lib/db'
+import parse from '../processor/parser'
+import * as fs from 'mz/fs'
 import Paragraph from '../processor/paragraph'
-import { Document, Schema } from 'mongoose'
+import { Document, Model, Schema } from 'mongoose'
 
 export interface Chapter extends Document {
   _id: number,
@@ -45,4 +47,25 @@ export const schema = new Schema({
   toJSON: { versionKey: false }
 })
 
-export default db.model<Chapter>('chapter', schema)
+interface ChapterMethods {
+  /** Export chapter to file */
+  export (this: Chapter): Promise<void>,
+
+  /** Synchronize chapter from file */
+  sync (this: Chapter): Promise<Chapter>
+}
+
+interface ChapterModel extends Model<Chapter>, ChapterMethods {}
+
+schema.methods = {
+  export () {
+    return fs.writeFile(this.file, Paragraph.generateSource(this.paragraphs))
+  },
+  async sync () {
+    const input = await fs.readFile(this.file, 'utf8')
+    this.paragraphs = parse(input)
+    return this.save()
+  }
+} as ChapterMethods
+
+export default db.model<Chapter, ChapterModel>('chapter', schema)
