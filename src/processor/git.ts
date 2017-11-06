8109146fed55e4cf6c89b4fdf1928d350058b6c6
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-06 17:00:43 +0800

    feat: add git helper

---
+++ src/processor/git.ts
@@ -0,0 +1,97 @@
+'use strict'
+
+import merge from './merge'
+import parse from './parser'
+import logger from '../lib/logger'
+import Chapter from '../models/chapter'
+import Paragraph from './paragraph'
+import * as path from 'path'
+import importFile from './import'
+import * as shelljs from 'shelljs'
+import { readFile } from 'mz/fs'
+import { MergeError } from '../lib/errors'
+
+const config = require('../../config')
+
+export default function git (...args: string[]) {
+  const gitArgs = [`--work-tree=${config.workDir}`, `--git-dir=${path.join(config.workDir, '.git')}`, ...args]
+  const gitArgsString = gitArgs.map(s => `"${s}"`).join(' ')
+  logger.debug('git', ...args)
+
+  return new Promise<string>((resolve, reject) => {
+    shelljs.exec('git ' + gitArgsString, (code, stdout, stderr) => {
+      if (code) {
+        logger.error(stderr || stdout)
+        reject(stderr || stdout)
+      } else {
+        resolve(stdout)
+      }
+    })
+  })
+}
+
+export async function currentBranch () {
+  const result = await git('branch')
+  return (result.split('\n').find(s => s.startsWith('*')) || '').slice(2)
+}
+
+export async function getRemoteChangesCount () {
+  const branch = await currentBranch()
+  const result = await git('rev-list', '--left-right', '--count', `${branch}...${config.remote}`)
+  const counts = result.split(/\s+/).map(s => Number(s))
+
+  return {
+    ahead: counts[0],
+    behind: counts[1]
+  }
+}
+
+export async function mergeRemote (message: string) {
+  // make sure that it is not in middle of a merge
+  await git('--no-pager', 'show', 'MERGE_HEAD')
+    .then(() => { throw new MergeError('There is another merge or rebase in progress') }, () => null)
+
+  try {
+    // ignore merge errors
+    await git('merge', config.remote, '--no-ff', '--no-commit').catch(() => null)
+
+    // get the changed files list
+    const files = (await git('status', '-s'))
+      .split('\n')
+      .map(s => s.split(/\s+/))
+
+    // merge all chapters
+    for (const [status, file] of files) {
+      if (!/^[012].*\.md$/.test(file)) continue
+
+      const fullPath = path.join(config.workDir, file)
+      const chapterId = parseInt(file, 10)
+
+      let paragraphs: Paragraph[] = []
+      if (status === 'UU') {
+        paragraphs = await merge(fullPath)
+      } else if (status === 'M' || status === 'A') {
+        const input = await readFile(fullPath, 'utf8')
+        paragraphs = parse(input)
+      }
+
+      const chapter = await Chapter.findById(chapterId)
+      if (chapter) {
+        await chapter.updateParagraphs(paragraphs)
+        await chapter.export()
+      } else {
+        // create new database record if the record is not exist
+        await importFile(fullPath)
+      }
+
+      await git('add', file)
+    }
+
+    // finally commit the merge
+    await git('commit', '-m', message)
+  } catch (err) {
+    // if there are error occurred, about the merge
+    await git('merge', '--abort')
+    throw err
+  }
+}
