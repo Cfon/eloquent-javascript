2e5d086318014e2690b73003d1695d1f80995360
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-07 16:00:22 +0800

    feat: add api to get specific paragraph of a chapter

--- src/routes/paragraph.ts
+++ src/routes/paragraph.ts
@@ -9,15 +9,15 @@ const PAGE_SIZE = 10
 
 router.get('chapter/:id/paragraphs/:page',
   async (ctx, next) => {
-    const id = Number(ctx.params.id)
+    const _id = Number(ctx.params.id)
     const page = Number(ctx.params.page)
 
-    if (Number.isNaN(id)) throw new ValidationFailureError('id 必须是一个数字')
+    if (Number.isNaN(_id)) throw new ValidationFailureError('id 必须是一个数字')
     if (Number.isNaN(page)) throw new ValidationFailureError('页码必须是一个数字')
     if (page < 1) throw new ValidationFailureError('页码必须大于等于 1')
 
     const result = await Chapter.aggregate([
-      { $match: { _id: id } },
+      { $match: { _id } },
       { $project: { _id: 0 } },
       {
         $project: {
@@ -41,8 +41,32 @@ router.get('chapter/:id/paragraphs/:page',
       }
     ])
 
-    if (result.length === 0) throw new NotFoundError(`没有 id 为 ${id} 的章节`)
+    if (result.length === 0) throw new NotFoundError(`没有 id 为 ${_id} 的章节`)
     ctx.result = result[0]
+  }
+)
+
+router.get('chapter/:id/paragraph/:pid',
+  async (ctx, next) => {
+    const _id = Number(ctx.params.id)
+    const { pid } = ctx.params
+
+    if (Number.isNaN(_id)) throw new ValidationFailureError('id 必须是一个数字')
+
+    const result: any = await Chapter.aggregate([
+      { $match: { _id } },
+      { $project: {
+        paragraphs: { $filter: { input: '$paragraphs', as: 'paragraph', cond: {
+          $eq: ['$$paragraph._id', pid]
+        } } }
+      } },
+      { $project: { data: { $arrayElemAt: ['$paragraphs', 0] } } }
+    ])
+
+    if (result[0] == null) throw new NotFoundError(`没有 id 为 ${_id} 的章节`)
+    if (result[0].data == null) throw new NotFoundError(`在第 ${_id} 章中没有 id 为 ${pid} 的段落`)
+
+    ctx.result = result[0].data
   }
 )
 
