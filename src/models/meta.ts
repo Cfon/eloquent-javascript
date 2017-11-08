4004e726d02dabd3b849e492ad7a064330f20739
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-08 15:16:23 +0800

    feat: add model 'meta' to store some key-value pair

---
+++ src/models/meta.ts
@@ -0,0 +1,36 @@
+'use strict'
+
+import db from '../lib/db'
+import { Document, Schema, SchemaTypes } from 'mongoose'
+
+interface Meta extends Document {
+  _id: string,
+  value: any
+}
+
+const schema = new Schema({
+  _id: String,
+  value: SchemaTypes.Mixed
+}, {
+  toObject: { versionKey: false },
+  toJSON: { versionKey: false }
+})
+
+const model = db.model<Meta>('meta', schema)
+
+export async function get (key: string, defaultValue?: any): Promise<any> {
+  const doc = await model.findById(key)
+  if (doc) return doc.toObject()
+  else if (defaultValue !== undefined) {
+    await set(key, defaultValue)
+    return defaultValue
+  } else return null
+}
+
+export function set (key: string, value: any) {
+  return model.findByIdAndUpdate(key, { $set: { value } }).exec()
+}
+
+export function remove (key: string) {
+  return model.findByIdAndRemove(key)
+}
