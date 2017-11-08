0306f038e0adc1153e5d775f24379d4c51bdbf83
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-08 14:36:27 +0800

    feat: update server startup script

--- src/main.ts
+++ src/main.ts
@@ -3,27 +3,28 @@
 import app from './app'
 import * as fs from 'fs'
 import * as http from 'http'
-import * as https from 'https'
 import logger from './lib/logger'
 
 const config = require('../config')
 const port = Number.parseInt(process.env.PORT || config.port || '3000')
+let server: http.Server
 
-const server = http.createServer(app.callback())
-server.listen(port)
-server.on('listening', () => {
-  logger.info(`Server started at port ${port}`)
-})
-
-// 确保错误日志通过 logger 输出
+// 确保错误日志通过 logger 输出，并结束进程
 function fatalErrorHandler (message: string) {
   return (err: Error) => {
     logger.fatal(message + ':')
     logger.fatal(err as any)
     server.close(() => process.exit(1))
+    setTimeout(() => process.exit(1), 1000)
   }
 }
 
-server.on('error', fatalErrorHandler('Error during server startup'))
-process.on('uncaughtException', fatalErrorHandler('Uncaught exception'))
-process.on('unhandledRejection', fatalErrorHandler('Unhandled promise rejection'))
+async function main () {
+  server = http.createServer(app.callback())
+  server.on('listening', () => logger.info(`Server started at port ${port}`))
+  server.on('error', fatalErrorHandler('HTTP 服务器错误'))
+  server.listen(port)
+}
+
+process.on('uncaughtException', fatalErrorHandler('发生未处理的异常'))
+process.on('unhandledRejection', fatalErrorHandler('发生未处理的 Promise rejection'))
