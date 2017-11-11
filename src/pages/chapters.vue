23122db37ede898d26e405091846552a799afb51
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-11 16:39:08 +0800

    fix: 在桌面环境下章节页面字体大小不统一

--- src/pages/chapters.vue
+++ src/pages/chapters.vue
@@ -7,9 +7,9 @@
       :to="'/chapters/' + chapter._id"
     >
       <v-list-tile-content>
-        <v-list-tile-title :class="{ 'body-2': chapter.unsaved }">
+        <v-list-tile-title :class="{ 'bold': chapter.unsaved }">
           <span>{{ chapter._id }}. {{ chapter.title }}</span><!--
-       --><span v-if="chapter.unsaved">*</span>
+      --><span v-if="chapter.unsaved">*</span>
         </v-list-tile-title>
         <v-list-tile-sub-title>{{ datetimeToString(chapter.updated) }}</v-list-tile-sub-title>
       </v-list-tile-content>
@@ -53,6 +53,10 @@
 
     .unsaved {
       background-color: fade($primary, 0.15);
+    }
+
+    .bold {
+      font-weight: bold;
     }
 
     .list__tile__title {
