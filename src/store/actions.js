5497dff1eb0df079d1217bd284fc0eb0ddc8c67c
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-11 11:11:53 +0800

    feat: 预留初始数据请求的错误处理

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,10 +7,15 @@ export default {
     const promise = Promise.all(['tags', 'chapters', 'origin'].map(s => axios.get(s)))
     commit('START_FETCH', promise)
 
-    const [tags, chapters, commitsBehind] = await promise
-    commit('SET_TAGS', tags.data)
-    commit('SET_CHAPTERS', chapters.data)
-    commit('SET_COMMITS_BEHIND', commitsBehind.data)
+    const result = await promise
+    if (result.every(item => item.status === 200)) {
+      const [tags, chapters, commitsBehind] = result
+      commit('SET_TAGS', tags.data)
+      commit('SET_CHAPTERS', chapters.data)
+      commit('SET_COMMITS_BEHIND', commitsBehind.data)
+    } else {
+      // TODO: 错误处理
+    }
 
     commit('FINISH_FETCH')
   }
