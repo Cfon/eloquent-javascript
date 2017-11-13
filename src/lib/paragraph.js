19cbb258026db950ee8575f936f4ac06e32b7ec9
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-13 11:04:07 +0800

    feat: 添加更多的元数据预览处理

--- src/lib/paragraph.js
+++ src/lib/paragraph.js
@@ -4,6 +4,8 @@ import render from './markdown'
 import escape from 'escape-html'
 import { parseData } from './markdown/markdown'
 
+import 'codemirror/lib/codemirror.css'
+
 function parseMeta (meta) {
   if (meta.startsWith('{{')) {
     if (meta.endsWith('}}')) {
@@ -62,14 +64,26 @@ export default async function generateDescription ({ source }) {
       }
     } else if (tag === 'quote') {
       data = args[0]
-      html = `<i>引用块开始</i>： ${data.author} ${data.title ? `&lt;${data.title}&gt;` : ''}`
+      html = `<i>引用块开始</i>：${data.author} ${data.title ? `&lt;${data.title}&gt;` : ''}`
+    } else if (tag === 'if') {
+      data = args[0]
+      html = `<i>条件渲染</i>：<code>${data}</code>`
+    } else if (tag === 'hint') {
+      html = `<i>代码提示开始</i>`
+    } else {
+      html = `<i>元数据 ${tag} 开始</i>`
     }
   } else if (source.endsWith('}}')) {
     const tag = source.slice(0, -2)
     type = 'meta'
+    data = null
 
     if (tag === 'quote') {
       html = `<i>引用块结束</i>`
+    } else if (tag === 'if') {
+      html = `<i>条件渲染结束</i>`
+    } else if (tag === 'hint') {
+      html = `<i>代码提示结束</i>`
     } else {
       html = `<i>元数据 ${tag} 结束</i>`
     }
