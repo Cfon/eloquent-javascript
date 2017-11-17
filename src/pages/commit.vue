1df5f7e57ae78f2949bd7d23eca67df3e462108d
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-17 14:41:56 +0800

    feat: 添加提交页面的API对接

--- src/pages/commit.vue
+++ src/pages/commit.vue
@@ -10,12 +10,12 @@
     <main>
       <v-content>
         <v-container>
-          <v-text-field v-model="data.message" placeholder="描述更改的内容" full-width single-line hide-details autofocus></v-text-field>
+          <v-text-field v-model="data.message" placeholder="描述更改的内容" :disabled="submitting" full-width single-line hide-details autofocus></v-text-field>
           <v-divider></v-divider>
           <v-list two-line>
-            <v-list-tile v-for="chapter in chapters" v-if="chapter.unsaved" :key="chapter._id" @click="toggle(chapter._id)" avatar ripple>
+            <v-list-tile v-for="chapter in chapters" v-if="chapter.unsaved" :key="chapter._id" @click="toggle(chapter._id)" :ripple="!submitting" avatar>
               <v-list-tile-action>
-                <v-checkbox :input-value="data.chapters" :value="chapter._id"></v-checkbox>
+                <v-checkbox :input-value="data.chapters" :value="chapter._id" color="accent" :disabled="submitting"></v-checkbox>
               </v-list-tile-action>
               <v-list-tile-content>
                 <v-list-tile-title class="body-1">{{ chapter._id }}. {{ chapter.title }}</v-list-tile-title>
@@ -26,7 +26,7 @@
         </v-container>
       </v-content>
       <v-fab-transition>
-        <v-btn v-if="!submitting" color="green" @click="submit" dark right bottom fab>
+        <v-btn v-if="!submitting && data.message.trim() && data.chapters.length" color="green" @click="submit" dark right bottom fab>
           <v-icon>check</v-icon>
         </v-btn>
       </v-fab-transition>
@@ -48,15 +48,26 @@
         this.data.chapters = this.chapters.filter(chapter => chapter.unsaved).map(chapter => chapter._id)
       },
       toggle (chapter) {
-        const index = this.data.chapters.indexOf(chapter)
-        if (index >= 0) {
-          this.data.chapters.splice(index, 1)
-        } else {
-          this.data.chapters.push(chapter)
+        if (!this.submitting) {
+          const index = this.data.chapters.indexOf(chapter)
+          if (index >= 0) {
+            this.data.chapters.splice(index, 1)
+          } else {
+            this.data.chapters.push(chapter)
+          }
         }
       },
       async submit () {
         this.submitting = true
+
+        const result = await this.$http.patch('origin', this.data)
+        if (!result.code) {
+          this.showSnackBar('提交成功')
+          this.fetchData()
+          this.$router.back()
+        } else this.showSnackBar(`提交失败：${result.message}`)
+
+        this.submitting = false
       }
     },
     beforeRouteLeave (to, from, next) {
