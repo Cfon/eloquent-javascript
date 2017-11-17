18ca9e50369085c76ecca8eb767ca9ceb4b595f7
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-17 15:30:31 +0800

    feat: 获取章节列表时排序

--- src/routes/chapter.ts
+++ src/routes/chapter.ts
@@ -8,16 +8,19 @@ const router = new Router()
 router.get('chapters',
   async (ctx, next) => {
     // 只保留章节的段落数
-    ctx.result = await Chapter.aggregate([{
-      $project: {
-        _id: 1,
-        title: 1,
-        updated: 1,
-        paragraphs: { $size: '$paragraphs' },
-        passed: { $size: { $filter: { input: '$paragraphs', as: 'p', cond: { $in: ['passed', '$$p.tags'] } } } },
-        unsaved: { $size: { $filter: { input: '$paragraphs', as: 'p', cond: { $eq: ['$$p.unsaved', true] } } } }
-      }
-    }])
+    ctx.result = await Chapter.aggregate([
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          updated: 1,
+          paragraphs: { $size: '$paragraphs' },
+          passed: { $size: { $filter: { input: '$paragraphs', as: 'p', cond: { $in: ['passed', '$$p.tags'] } } } },
+          unsaved: { $size: { $filter: { input: '$paragraphs', as: 'p', cond: { $eq: ['$$p.unsaved', true] } } } }
+        }
+      },
+      { $sort: { _id: 1 } }
+    ])
   }
 )
 
