647b0d0af92319f664ae531d0073601b289b519f
Jingkun Hua <huajingkun1214@gmail.com>
2017-11-18 21:15:44 +0800

    feat: 没有路由时重定向到 overview

--- src/lib/router.js
+++ src/lib/router.js
@@ -9,12 +9,13 @@ export default new VueRouter({
   base: '/eloquent-javascript/translator',
   mode: 'history',
   routes: [
-    { path: '/overview', component: require('../pages/overview').default, alias: '/' },
+    { path: '/overview', component: require('../pages/overview').default },
     { path: '/chapters', component: require('../pages/chapters').default },
     { path: '/chapter/:id', component: require('../pages/paragraphs').default },
     { path: '/chapter/:chapterId/paragraph/:paragraphId', component: require('../pages/paragraph').default },
     { path: '/commit', component: require('../pages/commit').default },
     { path: '/pull', component: require('../pages/pull').default },
-    { path: '/token', component: require('../pages/token').default }
+    { path: '/token', component: require('../pages/token').default },
+    { path: '*', redirect: '/overview' }
   ]
 })
